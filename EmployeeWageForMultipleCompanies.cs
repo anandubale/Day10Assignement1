ce856144101eb12bcb7fb901534de2bef22f234e
anandubale <au26199@gmail.com>
2021-12-21 00:28:55 +0530

    Added Program which will provide Emp wage for month of differet companies with different Parameters

---
+++ EmployeeWageForMultipleCompanies.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10EmployeeWageUc7_uc14
+{
+    class EmployeeWageForMultipleCompanies
+    {
+        //Fields
+
+
+        //Each COmpany has its own Wage Number of Working Days and Working Hours Per Month.
+        //use class method with function parameters instead of class variables.
+        int FullTime = 1;
+        int HalfTIme = 2;
+
+
+        int EmpRatePerHr;
+        int WorkingDaysPerMonth;
+        int EmpWorkingHrsPerMonth;
+        string Name;
+
+        //Constructor
+
+        public EmployeeWageForMultipleCompanies(string Name, int RatePerHr, int NoOfWorkingDays, int WorkingHrs)
+        {
+            this.Name = Name;
+            this.EmpRatePerHr = RatePerHr;
+            this.WorkingDaysPerMonth = NoOfWorkingDays;
+            this.EmpWorkingHrsPerMonth = WorkingHrs;
+
+        }
+
+        int WorkingHrs;
+        int empWage = 0;
+        Random random = new Random();
+        int totalEmpWage = 0;
+
+        //Methods
+        public void Solution()
+        {
+            int empWage;
+            int totalEmpWage = 0;
+            int empHrs = 0;
+            int totalWorlkingHr = 0;
+            int day = 0;
+            while (day <= WorkingDaysPerMonth || totalWorlkingHr <= EmpWorkingHrsPerMonth)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(1, 4);
+                switch (empCheck)
+                {
+                    case 1:
+                        Console.WriteLine("Employee is full Time");
+                        empHrs = 8;
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Employee is Part Time");
+
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+
+                        empHrs = 0;
+                        break;
+
+                }
+                empWage = empHrs * EmpRatePerHr;
+                totalEmpWage += empWage;
+                totalWorlkingHr += empHrs;
+                Console.WriteLine("Total Emp Hr : " + totalWorlkingHr);
+                Console.WriteLine("Employee Wage : " + empWage);
+
+                day++;
+
+                totalWorlkingHr++;
+
+
+            }
+            Console.WriteLine("For company Name : " + Name);
+
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            Console.WriteLine("Total Emp Hr : " + totalWorlkingHr);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Day10EmployeeWageUc7_uc14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Company Name: ");
+            string Name = Console.ReadLine();
+
+
+            Console.WriteLine("Enter Rate Per hour for Company");
+            int EmpRatePerHr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Woriking days per month");
+            int WorkingDaysPerMonth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter working hours per week");
+            int EmpWorkingHrs = int.Parse(Console.ReadLine());
+
+            EmployeeWageForMultipleCompanies c1 = new EmployeeWageForMultipleCompanies(Name, EmpRatePerHr, WorkingDaysPerMonth, EmpWorkingHrs);
+            c1.Solution();
+
+
+        }
+    }
+}
